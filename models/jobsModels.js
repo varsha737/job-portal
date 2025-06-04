634fd5a58b23485751eb598277ef42fceb2f5d05
Ritu-Varsha <rituvarsha7370@gmail.com>
2025-06-04 22:17:14 +0530

    Save changes before merging to main

--- models/jobsModels.js
+++ models/jobsModels.js
@@ -26,6 +26,9 @@ const jobSchema = new mongoose.Schema(
       default: "Mumbai",
       required: [true, "work location is required"],
     },
+    schedule: {
+      type: String,
+    },
     createdBy: {
       type: mongoose.Types.ObjectId,
       ref: "User",
